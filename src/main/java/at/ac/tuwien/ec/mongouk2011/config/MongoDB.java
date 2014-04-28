c8bd0431df454913c6226dc32c85076bd5b4810e
Philipp Krenn <pk@xeraa.net>
2014-04-28 19:23:00 +0200

    ADDED connection configuration options and saner error handling

--- src/main/java/at/ac/tuwien/ec/mongouk2011/config/MongoDB.java
+++ src/main/java/at/ac/tuwien/ec/mongouk2011/config/MongoDB.java
@@ -1,13 +1,14 @@
 package at.ac.tuwien.ec.mongouk2011.config;
 
-import java.util.logging.Logger;
-
 import at.ac.tuwien.ec.mongouk2011.entities.BaseEntity;
-
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import com.mongodb.WriteConcern;
+import java.net.UnknownHostException;
+import java.util.logging.Logger;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
-import com.mongodb.MongoClient;
-import com.mongodb.WriteConcern;
 
 /**
  * MongoDB providing the database connection.
@@ -20,16 +21,20 @@ public class MongoDB {
 	public static final String DB_NAME = "mongouk2011";
 
 	private MongoDB() {
-		try {
-			MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
-			mongoClient.setWriteConcern(WriteConcern.SAFE);
-			datastore = new Morphia().mapPackage(BaseEntity.class.getPackage().getName())
-					.createDatastore(mongoClient, DB_NAME);
-			datastore.ensureIndexes();
-			LOG.info("Connection to database '" + DB_NAME + "' initialized");
-		} catch (Exception e) {
+		MongoClientOptions mongoOptions = MongoClientOptions.builder().socketTimeout(60000)
+				.connectTimeout(1200000).build(); // SocketTimeout: 60s, ConnectionTimeout: 20min
+		MongoClient mongoClient;
+		try{
+			mongoClient = new MongoClient(new ServerAddress("127.0.0.1", 27017), mongoOptions);
+		} catch(UnknownHostException e){
 			throw new RuntimeException("Error initializing MongoDB", e);
 		}
+
+		mongoClient.setWriteConcern(WriteConcern.SAFE);
+		datastore = new Morphia().mapPackage(BaseEntity.class.getPackage().getName())
+				.createDatastore(mongoClient, DB_NAME);
+		datastore.ensureIndexes();
+		LOG.info("Connection to database '" + DB_NAME + "' initialized");
 	}
 
 	public static MongoDB instance() {
